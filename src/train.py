7a70866d24a425796ab6837d0adee44732caf3c3
ganeshmahadev <ganeshmahadev2463@gmail.com>
2024-11-03 23:39:24 +0100

    train pipeline

--- src/train.py
+++ src/train.py
@@ -1,39 +1,45 @@
-import pandas as pd 
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
-import mlflow 
 import yaml
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 from mlflow.models import infer_signature
-import os 
+import os
 
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split,GridSearchCV
 from urllib.parse import urlparse
 
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/ganeshmahadev2463/MLpipeline.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="ganeshmahadev2463"
+import mlflow
+
+os.environ['MLFLOW_TRACKING_URI']="https://dagshub.com/ganeshmahadev2463/MLpipeline.mlflow"
+os.environ['MLFLOW_TRACKING_USERNAME']="ganeshmahadev2463"
 os.environ["MLFLOW_TRACKING_PASSWORD"]="7cac48b10ad33dd2d44983a1db64f79c81101f43"
 
-def hyperparameter(X_train,y_train, param_grid):
+def hyperparameter_tuning(X_train,y_train,param_grid):
     rf=RandomForestClassifier()
-    grid_search=GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+    grid_search=GridSearchCV(estimator=rf,param_grid=param_grid,cv=3,n_jobs=-1,verbose=2)
     grid_search.fit(X_train,y_train)
     return grid_search
 
+## Load the parameters from params.yaml
+
 params=yaml.safe_load(open("params.yaml"))["train"]
 
-def train(data_path, model_path, random_state, n_estimators, max_depth):
+def train(data_path,model_path,random_state,n_estimators,max_depth):
     data=pd.read_csv(data_path)
     X=data.drop(columns=["Outcome"])
-    y=data["Outcome"]
+    y=data['Outcome']
 
-    mlflow.set_tracking_uri("MLFLOW_TRACKING_URI")
+    mlflow.set_tracking_uri("https://dagshub.com/ganeshmahadev2463/MLpipeline.mlflow")
 
+
+    ## start the MLFLOW run
     with mlflow.start_run():
-        X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.2)
+        # #split the dataset into training and test sets
+        X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.20)
         signature=infer_signature(X_train,y_train)
-    
-         ## Define hyperparameter grid
+
+        ## Define hyperparameter grid
 
         param_grid = {
             'n_estimators': [100, 200],
@@ -43,7 +49,7 @@ def train(data_path, model_path, random_state, n_estimators, max_depth):
         }
         
         # Perform hyperparameter tuning
-        grid_search=hyperparameter(X_train,y_train,param_grid)
+        grid_search=hyperparameter_tuning(X_train,y_train,param_grid)
 
         ## get the best model
         best_model=grid_search.best_estimator_
@@ -53,21 +59,22 @@ def train(data_path, model_path, random_state, n_estimators, max_depth):
         y_pred=best_model.predict(X_test)
         accuracy=accuracy_score(y_test,y_pred)
         print(f"Accuracy:{accuracy}")
-    
-         ## Log additional metrics \
+
+        ## Log additional metrics \
         mlflow.log_metric("accuracy",accuracy)
         mlflow.log_param("best_n_estimatios",grid_search.best_params_['n_estimators'])
         mlflow.log_param("best_max_depth", grid_search.best_params_['max_depth'])
         mlflow.log_param("best_sample_split", grid_search.best_params_['min_samples_split'])
         mlflow.log_param("best_samples_leaf", grid_search.best_params_['min_samples_leaf'])
-    
-        #log confusion metrics
+
+        ## log the confusion matrix and classification report
+
         cm=confusion_matrix(y_test,y_pred)
         cr=classification_report(y_test,y_pred)
 
         mlflow.log_text(str(cm),"confusion_matrix.txt")
-        mlflow.log_text(cr,"clasification_report.txt")
-    
+        mlflow.log_text(cr,"classification_report.txt")
+
         tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
 
         if tracking_url_type_store!='file':
@@ -75,7 +82,7 @@ def train(data_path, model_path, random_state, n_estimators, max_depth):
         else:
             mlflow.sklearn.log_model(best_model, "model",signature=signature)
 
-         ## create the directory to save the model
+        ## create the directory to save the model
         os.makedirs(os.path.dirname(model_path),exist_ok=True)
 
         filename=model_path
@@ -85,4 +92,11 @@ def train(data_path, model_path, random_state, n_estimators, max_depth):
 
 if __name__=="__main__":
     train(params['data'],params['model'],params['random_state'],params['n_estimators'],params['max_depth'])
+
+
+
+
+
+
+
 

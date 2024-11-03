a8a8a728ad3fa13a22d09ef5b9e034519eadb3a9
ganeshmahadev <ganeshmahadev2463@gmail.com>
2024-11-03 23:29:16 +0100

    train pipeline

--- src/preprocess.py
+++ src/preprocess.py
@@ -9,7 +9,7 @@ def preprocess(input_path, output_path):
     data=pd.read_csv(input_path)
 
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    data.to_csv(output_path, header=None, index=False)
+    data.to_csv(output_path, index=False)
     print(f"Preprocess data saved to {output_path}")
 
 
